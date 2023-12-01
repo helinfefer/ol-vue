4be335bf4be3d64881fda0110133371d7a097f14
sheyy <sheyueyu@qq.com>
2023-12-01 11:15:31 +0800

    主界面新增footer header

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,69 @@
 <template>
-  <div id="app" class="app">
-    <!-- <TopBar /> -->
-    <!-- <ParentOfMapSlideBar/> -->
-    <!-- <SidebarComponent/> -->
-    <!-- 指定组件的呈现位置 -->
+<el-container>
+	<el-aside width="200px"><SideBar/></el-aside>
+	<el-container>
+		<el-header style="text-align: right;">
+			<el-button @click="goBack">返回</el-button>
+			<el-button @click="goToInputPage">上传数据</el-button>
+		</el-header>
+		<el-main><router-view ></router-view></el-main>
+		<el-footer>Footer</el-footer>
+	</el-container>
+</el-container>
 
-
-
-    <!-- sidebar -->
-    <SideBar/>
-
-    <br>
-    <router-view ></router-view>
-  </div>
 </template>
 
 <script>
-// import ParentOfMapSlideBar from './components/ParentOfMapSlideBar.vue';
-// import TopBar from './components/TopBar.vue';
-// import SidebarComponent from './components/SidebarComponent.vue';
 import SideBar from './components/SideBar.vue';
-
-
 export default {
   name: 'App',
   components: {
-    // TopBar,
-    // SidebarComponent,
     SideBar,
-    // ParentOfMapSlideBar,
   },
+  methods: {
+		goToInputPage() {
+			if (this.$route.path !== '/ums/upload') {
+			this.$router.push('/ums/upload');
+			}
+		},
+		goBack() {
+			if (this.$route.path !== '/') {
+			this.$router.push('/');
+			}
+		}
+	}
 }
 </script>
 
 
 <style lang="scss">
-:root {
-	--primary-alt: #22c55e;
-	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
-	--sidebar-width: 300px;
+.el-header, .el-footer {
+    background-color: #e9ebed;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+	
+  }
+
+
+body > .el-container {
+margin-bottom: 40px;
+
 }
+
+.el-main {
+	padding: 0px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 400px;
+}
+
+
+.el-container:nth-child(7) .el-aside {
+    line-height: 400px;
+  }
 
 * {
 	margin: 0px;
@@ -50,6 +71,7 @@ export default {
 	box-sizing: border-box;
 	font-family: 'Fira sans', sans-serif;
 }
+
 
 body {
 	background: var(--light);
@@ -63,17 +85,4 @@ button {
 	background: none;
 }
 
-.app {
-	display: flex;
-
-	main {
-		flex: 1 1 0;
-		padding: 2rem;
-		min-width: 500px; 
-
-		@media (max-width: 768px) {
-			padding-left: 6rem;
-		}
-	}
-}
 </style>
