9d12c0c5706ee209c1e09e3d626b96fd22269dfe
sheyy <sheyueyu@qq.com>
2023-12-04 10:55:07 +0800

    删除文件列表控件ul

--- src/components/DataListComponent.vue
+++ src/components/DataListComponent.vue
@@ -3,18 +3,6 @@
 
     <el-row>
         <el-col :span="12">
-            <div>
-                <p>就业控制总量</p>
-                <ul>
-                    <li v-for="file in $store.state.uploadJobControlFileList" :key="file.uid" @click="displayContent(file)">
-                    {{ file.name }}
-                    </li>
-                </ul>
-                <p>家庭控制总量</p>
-                <li v-for="file in $store.state.uploadHouseholdsControlFileList" :key="file.uid">
-                    {{ file.name }}
-                </li>
-            </div>
 
         </el-col>
         <el-col :span="12">
@@ -51,22 +39,6 @@ export default {
     data() {
         return {
             // selectedFileContent: null,
-            options: [{
-                value: '选项1',
-                label: '黄金糕'
-                }, {
-                value: '选项2',
-                label: '双皮奶'
-                }, {
-                value: '选项3',
-                label: '蚵仔煎'
-                }, {
-                value: '选项4',
-                label: '龙须面'
-                }, {
-                value: '选项5',
-                label: '北京烤鸭'
-                }],
             uploadJobControlFile: '',
             uploadHouseholdsControlFile:'',
 
